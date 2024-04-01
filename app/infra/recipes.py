96ef650d7bd64b3efba6eb5aefdc970ac877ae03
Alex Karpodinis <alexander@karpodinis.net>
2024-04-01 18:52:51 -0400

    Create recipes with ingredient lists

--- app/infra/recipes.py
+++ app/infra/recipes.py
@@ -1,9 +1,11 @@
-from typing import Any, Optional
+from typing import Any
 from uuid import UUID
 
+from psycopg.errors import UniqueViolation
 from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 
-from app.infra.db import engine, recipes
+from app.infra.db import engine, ingredients, ingredients_recipes, recipes
 
 
 def all() -> list[dict[str, Any]]:
@@ -12,8 +14,62 @@ def all() -> list[dict[str, Any]]:
                 in conn.execute(recipes.select()).mappings().all()]
 
 
-def one(id: UUID) -> Optional[dict[str, Any]]:
+class RecipeDoesNotExistError(Exception):
+    pass
+
+
+def one(id: UUID) -> dict[str, Any]:
     with engine.connect() as conn:
-        ingredient = conn.execute(
-            select(recipes).where(recipes.c.id == id)).mappings().first()
-        return dict(ingredient) if ingredient else None
+        recipe = conn.execute(recipes.select().where(recipes.c.id == id)).mappings().first()
+        if not recipe:
+            raise RecipeDoesNotExistError
+
+        sel = select(ingredients.c.name, ingredients.c.id, ingredients_recipes.c.amount)
+        sel = sel.select_from(ingredients
+                              .join(ingredients_recipes,
+                                    ingredients_recipes.c.ingredient == ingredients.c.id)
+                              .join(recipes,
+                                    recipes.c.id == ingredients_recipes.c.recipe))
+        sel = sel.where(recipes.c.id == id)
+        rows = conn.execute(sel).mappings().all()
+
+        return {
+            'id': recipe.id,
+            'name': recipe.name,
+            'ingredients': [dict(row) for row in rows]
+        }
+
+
+class DuplicateRecipeError(Exception):
+    pass
+
+
+def insert(name: str, ingredients: list[tuple[UUID, int]]) -> UUID:
+    try:
+        with engine.connect() as conn:
+            # Insert the base recipe record, raising if there's a duplicate name
+            inserted = conn.execute(
+                recipes.insert().values(
+                    name=name
+                ).returning(
+                    recipes.c.id
+                )
+            ).mappings().first()
+
+            # Insert the join records with amounts included
+            for ingredient in ingredients:
+                conn.execute(
+                    ingredients_recipes.insert().values(
+                        recipe=inserted.id,
+                        ingredient=ingredient[0],
+                        amount=ingredient[1]
+                    )
+                )
+            conn.commit()
+    except IntegrityError as e:
+        if isinstance(e.orig, UniqueViolation):
+            raise DuplicateRecipeError
+        else:
+            raise
+
+    return inserted.id
