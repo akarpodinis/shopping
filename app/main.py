e05f1893e3a4c68e8268145ba6de6c90c0c702e6
Alex Karpodinis <alexander@karpodinis.net>
2024-04-02 18:48:40 -0400

    Index links to ingredients and recipes

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from app.routers import ingredients, recipes
+from app.routers import index, ingredients, recipes
 
 app = FastAPI()
 
+app.include_router(index.router)
 app.include_router(ingredients.router)
 app.include_router(recipes.router)
