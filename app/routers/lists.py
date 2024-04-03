3f5cb0a799bec88fd822a412cdf03fab0d87e1e6
Alex Karpodinis <alexander@karpodinis.net>
2024-04-03 17:13:09 -0400

    Make shopping lists from recipes

---
+++ app/routers/lists.py
@@ -0,0 +1,97 @@
+from datetime import datetime
+from typing import Annotated
+from uuid import UUID
+
+from fastapi import APIRouter, Form, Request, Response
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+
+from app.infra import recipes, lists
+
+router = APIRouter(prefix='/lists')
+
+templates = Jinja2Templates(directory='app/templates/lists')
+
+
+# Ordering matters here.  I want the router to check /ingredients/new on GET before trying to
+# figure out if the parameter is a UUID
+@router.get('/new')
+def new_form(request: Request) -> Response:
+    names = []
+    ids = []
+    for recipe in recipes.all():
+        names.append(recipe['name'])
+        ids.append(recipe['id'])
+
+    return templates.TemplateResponse(
+        request=request,
+        name='new.html',
+        context={
+            'recipe_names': names,
+            'recipe_ids': ids
+        }
+    )
+
+
+@router.post('')
+def add(date: Annotated[str, Form()],
+        recipes: Annotated[list[str], Form()],
+        scales: Annotated[list[str], Form()],
+        include_ingredients: Annotated[list[str], Form()],
+        arbitrary_names: Annotated[list[str], Form()],
+        arbitrary_aisles: Annotated[list[str], Form()],
+        arbitrary_amounts: Annotated[list[str], Form()]) -> Response:
+
+    # Filter empty arbitrary rows
+    arbitrary_items = []
+
+    for index, name in enumerate(arbitrary_names):
+        # Ignore the whole arbitrary item row if the name is blank
+        if not name:
+            continue
+        arbitrary_items.append((name, arbitrary_aisles[index], int(arbitrary_amounts[index])))
+
+    added_list = lists.add(
+        datetime.strptime(date, r'%Y-%m-%d'),
+        dict(zip([UUID(recipe) for recipe in recipes], [float(scale) for scale in scales])),
+        [UUID(ingredient) for ingredient in include_ingredients],
+        arbitrary_items
+    )
+
+    # Redirect to shopping list page
+    return RedirectResponse(f'/lists/{added_list}/shopping', status_code=303)
+
+
+@router.post('/confirm_stocked_and_arbitrary')
+def confirm_stocked(date: Annotated[str, Form()], included: Annotated[list[str], Form()],
+                    scales: Annotated[list[float], Form()], request: Request) -> Response:
+    ids = []
+    names = []
+    for id, name in recipes.stocked([UUID(recipe) for recipe in included]):
+        ids.append(id)
+        names.append(name)
+    return templates.TemplateResponse(
+        request=request,
+        name='confirm_stocked_and_arbitrary.html',
+        context={
+            'date': date,
+            'recipes': included,
+            'scales': scales,
+            'ids': ids,
+            'names': names
+        }
+    )
+
+
+@router.get('/{id}/shopping')
+def shopping(id: str, request: Request) -> Response:
+    shopping_list = lists.for_shopping(UUID(id))
+    print(shopping_list)
+    return templates.TemplateResponse(
+        request=request,
+        name='shopping.html',
+        context={
+            'date': shopping_list.date,
+            'aisles': shopping_list.aisles
+        }
+    )
