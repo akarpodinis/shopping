2ec28af7b8f73d94ec2e40877ba73d8c76efac4c
Alex Karpodinis <alexander@karpodinis.net>
2024-04-03 22:23:41 -0400

    Edit recipes, redirect to recipe list

--- app/infra/recipes.py
+++ app/infra/recipes.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import Any
 from uuid import UUID
 
@@ -38,6 +39,68 @@ def one(id: UUID) -> dict[str, Any]:
             'name': recipe.name,
             'ingredients': [dict(row) for row in rows]
         }
+
+
+def update(id: UUID, name: str, ingredients: list[tuple[UUID, int]]) -> None:
+    with engine.connect() as conn:
+        # Update the base record
+        conn.execute(
+            recipes.update().values(
+                name=name
+            ).where(recipes.c.id == id)
+        )
+
+        # Update the linked ingredients
+        for ingredient in ingredients:
+            conn.execute(
+                ingredients_recipes.update().values(
+                    amount=ingredient[1]
+                ).where(ingredients_recipes.c.ingredient == ingredient[0])
+            )
+        conn.commit()
+
+
+@dataclass
+class EditableIngredient:
+    id: str
+    name: str
+    amount: int
+
+
+@dataclass
+class EditableRecipe:
+    name: str
+    ingredients: list[EditableIngredient]
+
+
+def editable(id: UUID) -> EditableRecipe:
+    with engine.connect() as conn:
+        name = conn.execute(
+            select(recipes.c.name)
+            .where(recipes.c.id == id)
+        ).scalar()
+
+        """
+        select i.name, i.stocked, ir.amount from recipes r
+        join ingredients_recipes as ir on ir.recipe = r.id
+        join ingredients as i on i.id = ir.ingredient
+        where r.id = '86a37f9c-fbf7-426f-a54a-0eff25c26e18';
+        """
+        recipe_ingredients = conn.execute(
+            select(
+                ingredients.c.id,
+                ingredients.c.name,
+                ingredients.c.stocked,
+                ingredients_recipes.c.amount)
+            .join(ingredients_recipes, ingredients_recipes.c.recipe == recipes.c.id)
+            .join(ingredients, ingredients.c.id == ingredients_recipes.c.ingredient)
+            .where(recipes.c.id == id)
+        ).mappings().all()
+
+    return EditableRecipe(
+        name,
+        [EditableIngredient(ingredient.id, ingredient.name, ingredient.amount)
+         for ingredient in recipe_ingredients])
 
 
 class DuplicateRecipeError(Exception):
